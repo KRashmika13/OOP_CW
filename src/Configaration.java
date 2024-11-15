feeaaaf17e81d371d0bf82de7ff8fd1106d831bf
KRashmika13 <krashmika13@gmail.com>
2024-11-15 23:23:36 +0530

    modified main class

---
+++ src/Configaration.java
@@ -0,0 +1,18 @@
+import java.io.Serializable;
+
+public class Configaration implements Serializable {
+    private int totalTickets;
+    private int ticketReleaseRate;
+    private int customerRetrievalRate;
+    private int maxTicketCapacity;
+
+    public Configaration(int totalTickets, int ticketReleaseRate, int customerRetrievalRate, int maxTicketCapacity){
+        this.totalTickets = totalTickets;
+        this.ticketReleaseRate = ticketReleaseRate;
+        this.customerRetrievalRate = customerRetrievalRate;
+        this.maxTicketCapacity = maxTicketCapacity;
+    }
+
+
+
+}

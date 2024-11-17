b0883328fbbb90498c8392f5ae05e4366a4e5e0a
KRashmika13 <krashmika13@gmail.com>
2024-11-18 00:01:10 +0530

    added validation to validation checker class

--- src/ValidationaChecker.java
+++ src/ValidationaChecker.java
@@ -1,2 +1,25 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class ValidationaChecker {
+
+    public static int PositiveNumber(Scanner scan, String message){
+        int value = 0;
+        while(true){
+            System.out.println(message);
+            try{
+                value = scan.nextInt();
+                if(value < 0){
+                    throw new IllegalArgumentException("Value must be a positive number");
+                }
+                break;
+            }catch (InputMismatchException e){
+                System.err.print("Invalid input. Please enter valid number");
+            }catch (IllegalArgumentException e){
+                System.err.print(e.getMessage());
+            }
+        }
+        return value;
+    }
+
 }

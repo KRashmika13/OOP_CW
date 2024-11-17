e0cd754d8373d4eeaff61d7a09c096239cfe1698
KRashmika13 <krashmika13@gmail.com>
2024-11-18 00:00:27 +0530

    add validation to main class

--- src/Main.java
+++ src/Main.java
@@ -8,10 +8,10 @@ public class Main {
         System.out.println("Please enter the total number of tickets(): ");
         int totalTickets = scan.nextInt();
 
-        System.out.println("Please enter ticket release rate for vendor(): ");
+        System.out.print("Please enter ticket release rate for vendor(): ");
         int ticketReleaseRate = scan.nextInt();
 
-        System.out.println("Enter maximum ticket capacity ");
+        System.out.print("Enter maximum ticket capacity ");
         int maximumTicketCapacity = scan.nextInt();
 
 
